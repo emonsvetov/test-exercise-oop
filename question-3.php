fc7cf0e97355d1fca6524da1745e7c6e4001c957
Evgeny Monsvetov <emonsvetov@incento.com>
2022-11-11 17:29:12 -0600

    Small fixes.

--- question-3.php
+++ question-3.php
@@ -1,22 +1,29 @@
 <?php
 
-class Address{
-    private int $addrId;
+abstract class Model {
+    private string $id;
+
+    public function __construct()
+    {
+        $this->id = uniqid();
+    }
+
+    public function getId(){
+        return $this->id;
+    }
+}
+class Address extends Model{
     private string $details;
     private string $address;
     private float $lng;
     private float $lat;
-    public function __construct( int $addrId, string $address, string $details, float $lng, float $lat )
+    public function __construct(string $address, string $details, float $lng, float $lat )
     {
-        $this->addrId = $addrId;
+        parent::__construct();
         $this->details = $details;
         $this->address = $address;
         $this->lng = $lng;
         $this->lat = $lat;
-    }
-
-    public function getId(){
-        return $this->addrId;
     }
 
     /**
@@ -29,6 +36,11 @@ class Address{
         } else {
             return null;
         }
+    }
+
+    public function getAddress()
+    {
+        return $this->address;
     }
 }
 
@@ -48,7 +60,7 @@ class Package{
     }
 
     public function getWeight(){
-        return  $this->weight;
+        return $this->weight;
     }
 }
 
@@ -56,42 +68,38 @@ class ShippingAPI{
 
     public static function CalculateDeliveryCost( Address $destAddress, $weight ){
 
-        $workZone = $destAddress->getWorkZone();
+        // $workZone = $destAddress->getWorkZone();
         // call external api
         // curl ...
         //
 
-        return rand(5, 100);
+        return 14;
     }
 }
 
-class Customer{
+class Customer extends Model{
 
-    private int $customerId;
     private string $name;
     private array $addresses;
 
-    public function __construct(int $customerId, string $name )
+    public function __construct(string $name )
     {
-        $this->customerId = $customerId;
+        parent::__construct();
         $this->name = $name;
     }
 
     public function addAddress(Address $address){
 
-         //address calculation or we can use a separate ArrayOfAddresses object
+        //address calculation or we can use a separate ArrayOfAddresses object
         $this->addresses[$address->getId()] = $address;
-    }
-
-    public function getId(){
-        return $this->customerId;
     }
 
     /**
      * @description Customer Addresses
-     * @return array
+     * @param $addrId
+     * @return Address
      */
-    public function getAddress( $addrId ): array
+    public function getAddress( $addrId ): Address
     {
         //address calculation or we can use a separate ArrayOfAddresses object
         return $this->addresses[$addrId];
@@ -119,17 +127,16 @@ class Customer{
 }
 
 
-class Cart
+class Cart extends Model
 {
-    private string $id;
     private Customer $customer;
-    private Address $shipsToAddrId;
+    private string $shipsToAddrId;
 
-    public function __construct(Customer $customer, int $shipsToAddrId = 0)
+    public function __construct(Customer $customer, string $shipsToAddrId = '')
     {
+        parent::__construct();
         $this->customer = $customer;
         $this->shipsToAddrId = $shipsToAddrId;
-
         $this->items = [];
     }
 
@@ -164,11 +171,31 @@ class Cart
         $weight = 0;
         foreach ($items as $item) {
             $amount += $item->getCost();
-            $weight += $item->getDimensions()->getwWight();
+            $weight += $item->getDimensions()->getWeight();
         }
 
         $shippingRate = ShippingAPI::CalculateDeliveryCost( $this->getShippingqAddress(), $weight );
         return $amount + $shippingRate;
+    }
+
+    public function subTotal()
+    {
+        $amount = 0.00;
+        $items = $this->getItems();
+        foreach ($items as $item) {
+            $amount += $item->getPrice();
+        }
+        return $amount;
+    }
+
+    public function calcTax()
+    {
+        $tax = 0.00;
+        $items = $this->getItems();
+        foreach ($items as $item) {
+            $tax += floatval($item->getPrice() / 100 * $item->getTaxRate());
+        }
+        return $tax;
     }
 
     /**
@@ -184,8 +211,19 @@ class Cart
                 break;
             }
         }
-        $shippingRate = ShippingAPI::CalculateDeliveryCost( $this->getShippingqAddress(), $obj->getDimensions()->getwWight());
+
+        $shippingRate = ShippingAPI::CalculateDeliveryCost( $this->getShippingqAddress(), $obj->getDimensions()->getWeight());
         return $obj->getCost() + $shippingRate;
+    }
+
+    public function getShipping(){
+        $items = $this->getItems();
+        $weight = 0;
+        foreach ($items as $item) {
+            $weight += $item->getDimensions()->getWeight();
+        }
+
+        return ShippingAPI::CalculateDeliveryCost( $this->getShippingqAddress(), $weight );
     }
 
     public function setShippingqAddress($shipsToAddrId ){
@@ -193,21 +231,20 @@ class Cart
     }
 
     public function getShippingqAddress(){
-        return $this->customer->getAddress[$this->shipsToAddrId];
+        return $this->customer->getAddress($this->shipsToAddrId);
     }
 }
 
-class Item
+class Item extends Model
 {
-    private int $productId;
     private string $name;
     private float $price;
     private int $taxRate;
     private array $dimensions;
 
-    public function __construct(int $productId, string $name, float $price, array $dimensions=[], $taxRate=7)
+    public function __construct(string $name, float $price, array $dimensions=[], $taxRate=7)
     {
-        $this->productId = $productId;
+        parent::__construct();
         $this->name = $name;
         $this->price = $price;
         $this->dimensions = $dimensions;
@@ -227,7 +264,7 @@ class Item
         return $this->name;
     }
 
-    private function getTaxRate(): int
+    public function getTaxRate(): int
     {
         return $this->taxRate;
     }
@@ -241,35 +278,99 @@ class Item
         $price = (float)($this->price + floatval($this->price / 100 * $this->getTaxRate()));
         return $price;
     }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function display(){
+        return $this->name . ': $' . $this->price;
+    }
 }
 
-$customer = new Customer(uniqid(), 'Mike Tyson');
-
-$shipToAddrId = uniqid();
-$address = new Address($shipToAddrId, '14833 Hillside Trl, Savage, MN, 55378', 47.651968, 9.478485);
+$customer = new Customer('Mike Tyson');
+$address = new Address( '14833 Hillside Trl, Savage, MN, 55378','', 47.651968, 9.478485);
 $customer->addAddress($address);
-
-$cart = new Cart($customer, $shipToAddrId);
-$item = new Item(uniqid(), 'iPhone 14 Pro Max',  150.00, [150, 200, 20]);
-$item2 = new Item(uniqid(), 'iPhone 13 Pro Max',  15.00, [150, 200, 20]);
-
+$cart = new Cart($customer, $address->getId());
+$item = new Item( 'iPhone 14 Pro Max',  150.00, [150, 200, 20, 2]);
+$item2 = new Item( 'iPhone 13 Pro Max',  15.00, [150, 200, 20, 2]);
 $cart->addItem($item);
 $cart->addItem($item2);
 
-// Customer Name
-$cart->getCustomer()->getName();
+// console
+/**
+echo PHP_EOL;
+echo '1. Customer Name'.PHP_EOL;
+echo $cart->getCustomer()->getName();
+echo PHP_EOL.PHP_EOL;
 
-// Customer Addresses
-$cart->getCustomer()->getAddresses();
+echo '2. Customer Addresses'.PHP_EOL;
+print_r($cart->getCustomer()->getAllAddresses());
+echo PHP_EOL.PHP_EOL;
 
-// Items in Cart
-$cart->getItems();
+echo '3. Items in Cart'.PHP_EOL;
+print_r($cart->getItems());
+echo PHP_EOL.PHP_EOL;
 
-// Where Order Ships
-$cart->getShippingqAddress();
+echo '4. Where Order Ships'.PHP_EOL;
+print_r($cart->getShippingqAddress());
+echo PHP_EOL.PHP_EOL;
 
-// Cost of item in cart, including shipping and tax
-$cart->getItemCost($item->getId());
+echo '5. Cost of item in cart, including shipping and tax'.PHP_EOL;
+echo '$'.$cart->getItemCost($item->getId());
+echo PHP_EOL;
+echo '$'.$cart->getItemCost($item2->getId());
+echo PHP_EOL.PHP_EOL;
 
-// Total for all items
-$cart->total();
+echo '6. Total for all items'.PHP_EOL;
+echo '$'.$cart->total();
+echo PHP_EOL.PHP_EOL;
+ *
+ *
+ */
+// web
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8"/>
+    <title>Question 3</title>
+</head>
+<body style="padding: 15px; border: 1px solid #ccc;">
+
+    <p><b>1. Customer Name</b></p>
+    <p><?php echo $cart->getCustomer()->getName(); ?></p>
+    <div>&nbsp;</div>
+
+    <p><b>2. Customer Addresses</b></p>
+    <?php foreach($cart->getCustomer()->getAllAddresses() as $addressItem): ?>
+        <p><?php echo $addressItem->getAddress(); ?></p>
+    <?php endforeach; ?>
+    <div>&nbsp;</div>
+
+    <p><b>3. Items in Cart</b></p>
+    <?php foreach($cart->getItems() as $cartItem): ?>
+        <p><?php echo $cartItem->display(); ?></p>
+    <?php endforeach; ?>
+    <div>&nbsp;</div>
+
+    <p><b>4. Where Order Ships</b></p>
+    <p><?php echo $cart->getShippingqAddress()->getAddress(); ?></p>
+    <div>&nbsp;</div>
+
+    <p><b>5. Cost of item in cart, including shipping and tax</b></p>
+    <p>$<?php echo $cart->getItemCost($item->getId()); ?></p>
+    <p>$<?php echo $cart->getItemCost($item2->getId()); ?></p>
+    <div>&nbsp;</div>
+
+    <p><b>6. Subtotal and total for all items</b></p>
+    <p>Items Subtotal: $<?php echo $cart->subTotal(); ?></p>
+    <p>Shipping: $<?php echo $cart->getShipping(); ?></p>
+    <p>Total before tax: $<?php echo $cart->subTotal() + $cart->getShipping(); ?></p>
+    <p>Estimated tax to be collected: $<?php echo $cart->calcTax(); ?></p>
+    <p>Grand Total: $<?php echo $cart->total(); ?></p>
+    <div>&nbsp;</div>
+
+</body>
+</html>
